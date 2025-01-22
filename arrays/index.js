0ef5617c1d27d84bf1fa4281fe1496961a8a0e91
u4ik <insighteuphoric@gmail.com>
2025-01-21 20:04:49 -0500

    added basic array notes

--- arrays/index.js
+++ arrays/index.js
@@ -8,19 +8,18 @@
 //             0       1         2          3
 let list = ["milk", "bread", "chicken", "coffee"];
 
-
 console.log(list[2]); // output: chicken
 
 // Checking length
 
-console.log(list.length)
+console.log(list.length);
 
 // Getting last element of array
-console.log(list[list.length - 1]) // output: coffee
+console.log(list[list.length - 1]); // output: coffee
 
-let avengers = ["Nick", 2, false, "Steve", "Bruce", ["Tony", 8, true]]
+let avengers = ["Nick", 2, false, "Steve", "Bruce", ["Tony", 8, true]];
 
-console.log(typeof avengers) // Wont work
+console.log(typeof avengers); // Wont work
 console.log(avengers instanceof Array); // true if array
 
 /* 
@@ -30,11 +29,11 @@ console.log(avengers instanceof Array); // true if array
     - Using string interpolation, log "Hello, Nick" and another "Hello, Tony"
 */
 
-console.log(avengers[4]) // Bruce
+console.log(avengers[4]); // Bruce
 
 console.log(avengers[5][2]); // true
 
-console.log(`Hello, ${avengers[0]}` , `Hello, ${avengers[5][0]}`); // Hello, Nick Hello, Tony
+console.log(`Hello, ${avengers[0]}`, `Hello, ${avengers[5][0]}`); // Hello, Nick Hello, Tony
 
 /* 
     ? Reference
@@ -43,12 +42,64 @@ console.log(`Hello, ${avengers[0]}` , `Hello, ${avengers[5][0]}`); // Hello, Nic
         - It can be important when working with immutable data (data that shouldn't change), that we not alter things in which or where they originate
 */
 
-let myPublicInfo = [ "Amit", 34 ];
+// Creating an array to house public info that will be seen by all users
+let myPublicInfo = ["Amit", 34];
+// Creating a new variable to house private info... but it still refers to same array held in memory
+let myPrivateInfo = myPublicInfo;
+// Some private information being pushed to the array
+myPrivateInfo.push("555-55-5555");
 
-let myPrivateInfo = myPublicInfo
+console.log(myPrivateInfo); // Has the private info... but
+console.log(myPublicInfo); //! Private data shows up in original! Because we were modifying the same array held in memory
 
-myPrivateInfo.push("555-55-5555")
+//? Array Methods
+let food = ["Pecan pie", "Shrimp", "Quesadilla", "Cheese Cake", "Hotdog"];
+// *.push() - adds an item to the end of an array
+food.push("Pizza");
+console.log("PUSH:", food);
 
-console.log(myPrivateInfo)
+// *.splice(target position, how many removed, replace with items here)
+food.splice(1, 1, "Tacos");
 
-console.log(myPublicInfo);
+food.splice(1, 0, "Steak");
+
+console.log("SPLICE:", food);
+
+// *.slice() - Return what is 'sliced' from the array. Starting index, and the index of the item before you end
+
+let sliced = food.slice(1, 3);
+
+console.log(sliced);
+
+// *.pop() - Removes the last element from the array, returns the last item
+console.log(food.pop());
+
+console.log("POP:", food);
+
+// *.shift // Removes an item from the beginning of the array
+
+food.shift();
+
+console.log("SHIFT", food);
+
+// *.unshift // Inserts new elements to the start of the array
+food.unshift("Hamburger", "Salad");
+
+console.log("UNSHIFT:", food);
+
+// *.toString()
+
+let rgb = ["red", "green", "blue"];
+console.log(rgb.toString());
+console.log(typeof rgb.toString());
+
+// *.split - Transforms a string into an array
+
+let string = "Amit Singh Mangat";
+
+console.log(string.split(" "))
+
+// *.join - Transforms an array into a string
+let sentence = ["My", "name", "is", "Bob."]
+
+console.log(sentence.join(" "));
